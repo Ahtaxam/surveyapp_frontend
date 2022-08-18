96f3db4f0c4b9a9104a23b9d29b9406a7fd34149
Ahtaxam <64573767+Ahtaxam@users.noreply.github.com>
2022-08-18 20:58:54 +0500

    refactor: made changes change file path

--- src/PrivateRoutes.js
+++ src/PrivateRoutes.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import PATH from "./Path";
+import PATH from "./Constants/Path";
 
 function PrivateRoutes({ component: Component, ...restOfProps }) {
   const token = document.cookie.split("=")[1];
   if (!token) {
-    return <Navigate to={PATH.login} />;
+    return <Navigate to={PATH.LOGIN} />;
   } else {
     return <Component />;
   }
