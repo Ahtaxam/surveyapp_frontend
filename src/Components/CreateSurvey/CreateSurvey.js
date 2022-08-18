87603a3dd3466364ed9fb08e75e2c7b3a840ac31
Ahtaxam <64573767+Ahtaxam@users.noreply.github.com>
2022-08-18 14:55:56 +0500

    feat: add back button

--- src/Components/CreateSurvey/CreateSurvey.js
+++ src/Components/CreateSurvey/CreateSurvey.js
@@ -1,10 +1,17 @@
 import { TextField } from "@mui/material";
-
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import React from "react";
 
 import "./createsurvey.css";
 import SurveyQuestions from "./SurveyQuestions";
+import PATH from "../../Path";
+
 function CreateSurvey() {
+  const navigate = useNavigate();
+  const backToDashBoard = () => {
+    navigate(PATH.dashboard);
+  };
   return (
     <div>
       <section className="surveyheader">
@@ -15,15 +22,31 @@ function CreateSurvey() {
         />
         <TextField
           id="filled-textarea"
-          placeholder="Form description"
+          placeholder="Form Description"
           multiline
           variant="standard"
         />
       </section>
 
-      <section className="">
+      <section>
         <SurveyQuestions />
       </section>
+      <Button
+        id="submit-button"
+        variant="contained"
+        style={{ textAlign: "center", width: "180px" }}
+      >
+        Submit
+      </Button>
+
+      <Button
+        id="back-button"
+        variant="contained"
+        style={{ textAlign: "center", width: "180px" }}
+        onClick={backToDashBoard}
+      >
+        Back
+      </Button>
     </div>
   );
 }
