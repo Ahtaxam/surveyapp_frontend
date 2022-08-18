b741f407764965ede2c372550c222e1114a2eddc
Ahtaxam <64573767+Ahtaxam@users.noreply.github.com>
2022-08-18 11:34:48 +0500

    feat: add create survey route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import { Routes, Route } from "react-router-dom";
+
 import DashBoard from "./Components/Dashboard/DashBoard";
-import Path from "./Path";
+import PATH from "./Path";
 import Home from "./Components/HomeComponent/Home";
 import Login from "./Components/LoginComponent/Login";
 import Signup from "./Components/SignupComponent/Signup";
 import PrivateRoutes from "./PrivateRoutes";
+import CreateSurvey from "./Components/CreateSurvey/CreateSurvey";
+
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path={Path.home} element={<Home />} />
-        <Route path={Path.login} element={<Login />} />
-        <Route path={Path.signup} element={<Signup />} />
+        <Route path={PATH.home} element={<Home />} />
+        <Route path={PATH.login} element={<Login />} />
+        <Route path={PATH.signup} element={<Signup />} />
         <Route
-          path={Path.dashboard}
+          path={PATH.dashboard}
           element={<PrivateRoutes component={DashBoard} />}
+        />
+        <Route
+          path={PATH.createsurvey}
+          element={<PrivateRoutes component={CreateSurvey} />}
         />
       </Routes>
     </div>
