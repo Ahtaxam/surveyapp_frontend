7a45f3f5c42bdeabb6447f811c6d5482d77622f8
Ahtaxam <64573767+Ahtaxam@users.noreply.github.com>
2022-08-22 16:37:38 +0500

    refactor: made changes and lift state up

--- src/Components/CreateSurvey/CreateSurvey.js
+++ src/Components/CreateSurvey/CreateSurvey.js
@@ -1,18 +1,106 @@
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 
 import "./createsurvey.css";
 import SurveyQuestions from "./SurveyQuestions";
 import PATH from "../../Constants/Path";
 import Navbar from "../Navbar/Navbar";
 
+import QUESTION_TYPE from "../../Constants/QUESTIONS_TYPES";
+
 function CreateSurvey() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [userForms, setUserForms] = useState([
+    {
+      title: "",
+      type: QUESTION_TYPE.MULTIPLECHOICE,
+      options: ["option1"],
+    },
+  ]);
   const navigate = useNavigate();
   const backToDashBoard = () => {
     navigate(PATH.DASHBOARD);
   };
+
+  const submitSurvey = (questions) => {
+    console.log("submit survey");
+  };
+
+  // this function is used to add question title to the survey
+  const handleQuestion = (value, index) => {
+    const userForm = [...userForms];
+    userForm[index].title = value;
+    setUserForms(userForm);
+  };
+
+  // this function set options selected type for each question
+  const handleQuestionType = (selectedType, questionNo) => {
+    const userForm = [...userForms];
+    userForm[questionNo].type = selectedType;
+    if (selectedType === QUESTION_TYPE) {
+      userForm[questionNo].options = ["number"];
+    } else if (selectedType === QUESTION_TYPE.TEXT) {
+      userForm[questionNo].options = ["short text"];
+    } else {
+      userForm[questionNo].options = ["option1"];
+    }
+    setUserForms(userForm);
+  };
+
+  // function to delete a question from survey
+  const handleDeleteQuestion = (index) => {
+    const userForm = [...userForms];
+    userForm.splice(index, 1);
+    setUserForms(userForm);
+  };
+
+  // function to add new question to survey
+  const handleAddQuestion = () => {
+    const userForm = [...userForms];
+    userForm.push({
+      title: "",
+      type: QUESTION_TYPE.MULTIPLECHOICE,
+      options: ["option1"],
+    });
+    setUserForms(userForm);
+  };
+
+  // function to add new option to a question
+  const handleAddOption = (questionNo, length) => {
+    const userForm = [...userForms];
+    userForm[questionNo].options.push("option" + (length + 1));
+    setUserForms(userForm);
+  };
+
+  //function to add a particular option value to a question
+  const handleOptionValue = (values) => {
+    const { value, index, cardNo } = values;
+    const userForm = [...userForms];
+    userForm[cardNo].options[index] = value;
+    setUserForms(userForm);
+  };
+
+  // function to delete an option from a question
+  const handleDeleteOption = (index, questionNo) => {
+    const userForm = [...userForms];
+    userForm[questionNo].options.splice(index, 1);
+    setUserForms(userForm);
+  };
+
+  // function to copy a question from survey
+  const copyQuestion = (QuestionIndex) => {
+    const userForm = [...userForms];
+    userForm.splice(QuestionIndex + 1, 0, {
+      ...userForm[QuestionIndex],
+    });
+
+    userForm[QuestionIndex + 1].options = [...userForm[QuestionIndex].options];
+    setUserForms(userForm);
+  };
+
   return (
     <div>
       <Navbar />
@@ -21,17 +109,31 @@ function CreateSurvey() {
           type="text"
           className="surveyheader__input"
           placeholder="UNTITLED SURVEY"
+          value={title}
+          onChange={(e) => setTitle(e.target.value.toUpperCase())}
         />
         <TextField
           id="filled-textarea"
           placeholder="Form Description"
           multiline
           variant="standard"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
         />
       </section>
 
       <section>
-        <SurveyQuestions />
+        <SurveyQuestions
+          questions={userForms}
+          handleQuestion={handleQuestion}
+          handleQuestionType={handleQuestionType}
+          handleAddOption={handleAddOption}
+          handleOptionValue={handleOptionValue}
+          handleDeleteOption={handleDeleteOption}
+          handleDeleteQuestion={handleDeleteQuestion}
+          handleAddQuestion={handleAddQuestion}
+          copyQuestion={copyQuestion}
+        />
       </section>
       <Button
         id="submit-button"
@@ -41,6 +143,7 @@ function CreateSurvey() {
           backgroundColor: "#f50057",
           color: "white",
         }}
+        onClick={submitSurvey}
       >
         Save
       </Button>
