49179b4aaf18eaa6dede7544c6151213ff0f62e4
Ahtaxam <64573767+Ahtaxam@users.noreply.github.com>
2022-08-23 14:45:10 +0500

    refactor: made changes for validation

--- src/Components/CreateSurvey/validateSurveyQuestions.js
+++ src/Components/CreateSurvey/validateSurveyQuestions.js
@@ -1,9 +1,17 @@
+import QUESTIONS_TYPE from "../../Constants/QUESTIONS_TYPES";
+
 export function validateSurveyQuestions(surQuestions) {
+  console.log("surQuestions", surQuestions);
   const titles = [];
   const option = [];
   surQuestions.forEach((question) => {
     titles.push(question.title);
-    option.push(question.options[0]);
+    if (
+      question.type === QUESTIONS_TYPE.CHECKBOX ||
+      question.type === QUESTIONS_TYPE.MULTIPLECHOICE
+    ) {
+      option.push(question.options[0]);
+    }
   });
   const titleResult = titles.every((ti) => ti.length > 0);
   const optionResult = option.every((ti) => ti.length > 0);
