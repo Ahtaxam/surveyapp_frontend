24bac24570a497f1bdc43dce9c7d64dad5ac8f0f
Ahtaxam <64573767+Ahtaxam@users.noreply.github.com>
2022-08-25 18:07:11 +0500

    refactor: made changes rename viriable name

--- src/Components/EditSurvey/EditOptions.js
+++ src/Components/EditSurvey/EditOptions.js
@@ -34,12 +34,12 @@ function EditOptions({
     <div>
       {options.map((option, index) => (
         <div key={index} style={{ display: "flex", gap: "10px" }}>
-          {[QUESTIONS_TYPE.CHECKBOX, QUESTIONS_TYPE.MULTIPLECHOICE].includes(
+          {[QUESTIONS_TYPE.CHECKBOX, QUESTIONS_TYPE.MULTIPLE_CHOICE].includes(
             selectedType
           ) && (
             <input
               type={
-                selectedType === QUESTIONS_TYPE.MULTIPLECHOICE
+                selectedType === QUESTIONS_TYPE.MULTIPLE_CHOICE
                   ? "radio"
                   : selectedType
               }
@@ -48,7 +48,7 @@ function EditOptions({
             />
           )}
           &nbsp;&nbsp;&nbsp;
-          {[QUESTIONS_TYPE.CHECKBOX, QUESTIONS_TYPE.MULTIPLECHOICE].includes(
+          {[QUESTIONS_TYPE.CHECKBOX, QUESTIONS_TYPE.MULTIPLE_CHOICE].includes(
             selectedType
           ) ? (
             <TextField
@@ -71,7 +71,7 @@ function EditOptions({
         </div>
       ))}
 
-      {[QUESTIONS_TYPE.CHECKBOX, QUESTIONS_TYPE.MULTIPLECHOICE].includes(
+      {[QUESTIONS_TYPE.CHECKBOX, QUESTIONS_TYPE.MULTIPLE_CHOICE].includes(
         selectedType
       ) && (
         <Button
