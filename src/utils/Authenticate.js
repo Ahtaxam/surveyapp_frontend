561a26dda9559d8ccbc4da3bb884fe0bf37cb5bf
Ahtaxam <64573767+Ahtaxam@users.noreply.github.com>
2022-08-31 20:54:32 +0500

    feat: fetch token from cookie to authenticate

---
+++ src/utils/Authenticate.js
@@ -0,0 +1,5 @@
+export const tokenName = "expressToken";
+export const authToken = () =>
+  document.cookie.match(new RegExp("(^| )" + tokenName + "=([^;]+)")) !== null
+    ? document.cookie.match(new RegExp("(^| )" + tokenName + "=([^;]+)"))[2]
+    : null; // eslint-disable-line
