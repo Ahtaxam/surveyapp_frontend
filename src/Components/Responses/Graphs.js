e834a3f3d053e65c1b7d89623f3a5837af0abc9b
Ahtaxam <64573767+Ahtaxam@users.noreply.github.com>
2022-09-07 19:01:43 +0500

    refactor: made changes

--- src/Components/Responses/Graphs.js
+++ src/Components/Responses/Graphs.js
@@ -4,35 +4,14 @@ import { styled, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
-import { Bar } from "react-chartjs-2";
 import { Link } from "react-router-dom";
 import Box from "@mui/material/Box";
 import { toast } from "react-toastify";
 import fileDownload from "js-file-download";
-import ButtonGroup from "@mui/material/ButtonGroup";
-
+import ShowGraph from "./ShowGraph";
 import { authToken } from "../../utils/Authenticate";
 import PATH from "../../Constants/Path";
 import "./graph.css";
-import {
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  BarElement,
-  Title,
-} from "chart.js";
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
-ChartJS.register(ArcElement, Tooltip, Legend);
 
 /**
  *
@@ -40,20 +19,18 @@ ChartJS.register(ArcElement, Tooltip, Legend);
  */
 function Graphs() {
   const [responses, setResponses] = useState([]);
-  const [Questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState([]);
+  const [surveyName, setSurveyName] = useState("");
   const [isError, setIsError] = useState("");
   const { surveyId } = useParams();
 
   const fetchResponses = useCallback(() => {
-    const options = {
-      method: "GET",
-      url: `${process.env.REACT_APP_BASE_URL}${PATH.RESPONSES}/${surveyId}`,
-      headers: {
-        config: `Bearer ${authToken()}`,
-      },
-    };
     axios
-      .request(options)
+      .get(`${process.env.REACT_APP_BASE_URL}${PATH.RESPONSES}/${surveyId}`, {
+        headers: {
+          config: `Bearer ${authToken()}`,
+        },
+      })
       .then((response) => {
         setResponses(response.data);
       })
@@ -63,16 +40,14 @@ function Graphs() {
   }, [surveyId]);
 
   const fetchQuestions = useCallback(() => {
-    const options = {
-      method: "GET",
-      url: `${process.env.REACT_APP_BASE_URL}${PATH.SURVEY}/${surveyId}`,
-      headers: {
-        config: `Bearer ${authToken()}`,
-      },
-    };
     axios
-      .request(options)
+      .get(`${process.env.REACT_APP_BASE_URL}${PATH.SURVEY}/${surveyId}`, {
+        headers: {
+          config: `Bearer ${authToken()}`,
+        },
+      })
       .then((response) => {
+        setSurveyName(response.data.name);
         setQuestions(response.data.questions);
       })
       .catch((error) => {
@@ -81,21 +56,17 @@ function Graphs() {
   }, [surveyId]);
 
   const downloadReport = () => {
-    const options = {
-      method: "GET",
-      url: `${process.env.REACT_APP_BASE_URL}/download/${surveyId}`,
-      headers: {
-        config: `Bearer ${authToken()}`,
-      },
-    };
     axios
-      .request(options)
+      .get(`${process.env.REACT_APP_BASE_URL}/download/${surveyId}`, {
+        headers: {
+          config: `Bearer ${authToken()}`,
+        },
+      })
       .then((response) => {
-        toast.success("Report Downloaded Successfully");
-        fileDownload(response.data, "report.csv");
+        fileDownload(response.data, `${surveyName}.csv`);
       })
       .catch((error) => {
-        toast.error(error.message);
+        toast.error("Something went wrong");
       });
   };
 
@@ -106,7 +77,7 @@ function Graphs() {
   return (
     <div>
       {!isError && responses.length > 0 && (
-        <Box textAlign="center">
+        <Box textAlign="center" sx={{mt:2}}>
           <Button variant="contained" onClick={downloadReport}>
             Download report
           </Button>
@@ -122,7 +93,7 @@ function Graphs() {
             </CardContent>
           </Card>
           <div id="graphdiv">
-            {Questions.map((question, index) => (
+            {questions.map((question, index) => (
               <div key={index}>
                 <Card>
                   <CardContent>
@@ -130,7 +101,7 @@ function Graphs() {
                   </CardContent>
                   <ShowGraph
                     responses={responses}
-                    Questions={Questions}
+                    questions={questions}
                     index={index}
                     type={question.type}
                     options={question.options}
@@ -176,155 +147,3 @@ const Card = styled(MuiCard)({
 });
 
 export default Graphs;
-
-/**
- *
- * @param {object[]} responses
- * @param {Number} index
- * @param {String} type
- * @param {string[]} options
- * @param {String} activeButton
- * @returns {Component} render graph
- *
- */
-function ShowGraph({ responses, index, Questions, type, options }) {
-  const [activeButton, setActiveButton] = useState("Bar");
-  const handleType = (value) => {
-    setActiveButton(value);
-  };
-  let result = [];
-
-  const colors = [
-    "#5F6F94",
-    "#A7D2CB",
-    "#0F3460",
-    "#42032C",
-    "#A66CFF",
-    "#FF6384",
-    "#36A2EB",
-    "#486f27",
-    "#FFCE56",
-    "#5F6F94",
-  ];
-  responses.forEach((response) => {
-    result.push(...response.answers[index].options);
-  });
-  if (type === "multiplechoice") {
-    const d = [];
-    for (let i of options) {
-      const index = result.indexOf(i);
-      if (index < 0) {
-        d.push(0);
-      } else {
-        d.push(result.filter((x) => x === i).length);
-      }
-    }
-    const data = {
-      labels: options,
-      datasets: [
-        {
-          label: "Responses",
-          data: d,
-          backgroundColor: [
-            "#FF6384",
-            "#36A2EB",
-            "#486f27",
-            "#FFCE56",
-            "#5F6F94",
-          ],
-          borderWidth: 1,
-        },
-      ],
-    };
-    let optionss = {
-      maintainAspectRation: false,
-    };
-    return (
-      <div id="graph">
-        {activeButton === "Bar" ? (
-          <Bar data={data} options={optionss} />
-        ) : (
-          <Doughnut data={data} options={optionss} />
-        )}
-        <div className="btndiv">
-          <ButtonGroup
-            variant="contained"
-            aria-label="outlined primary button group"
-          >
-            <Button
-              color={activeButton === "Bar" ? "secondary" : "primary"}
-              onClick={() => handleType("Bar")}
-            >
-              Bar
-            </Button>
-            <Button
-              color={activeButton === "Doughnut" ? "secondary" : "primary"}
-              onClick={() => handleType("Doughnut")}
-            >
-              Doughnut
-            </Button>
-          </ButtonGroup>
-        </div>
-      </div>
-    );
-  }
-
-  if (type === "text" || type === "number" || type === "checkbox") {
-    let obj = {};
-    for (let i of result) {
-      if (obj[i]) {
-        obj[i] += 1;
-      } else {
-        obj[i] = 1;
-      }
-    }
-    const data = {
-      labels: Object.keys(obj),
-      datasets: [
-        {
-          label: "count",
-          data: Object.values(obj),
-          borderColor: "rgb(255, 99, 132)",
-          backgroundColor:
-            colors[
-              (Math.floor(Math.random() * colors.length),
-              Math.floor(Math.random() * colors.length),
-              Math.ceil(Math.random() * colors.length))
-            ],
-          borderWidth: 1,
-        },
-      ],
-    };
-    let optionss = {
-      maintainAspectRation: false,
-    };
-    return (
-      <div id="graph">
-        {activeButton === "Bar" ? (
-          <Bar data={data} options={optionss} />
-        ) : (
-          <Doughnut data={data} options={optionss} />
-        )}
-        <div className="btndiv">
-          <ButtonGroup
-            variant="contained"
-            aria-label="outlined primary button group"
-          >
-            <Button
-              color={activeButton === "Bar" ? "secondary" : "primary"}
-              onClick={() => handleType("Bar")}
-            >
-              Bar
-            </Button>
-            <Button
-              color={activeButton === "Doughnut" ? "secondary" : "primary"}
-              onClick={() => handleType("Doughnut")}
-            >
-              Doughnut
-            </Button>
-          </ButtonGroup>
-        </div>
-      </div>
-    );
-  }
-}
