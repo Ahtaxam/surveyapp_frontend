3b8ee7edb169a11e2c58f7af90d7a9803274b5b4
Ahtaxam <64573767+Ahtaxam@users.noreply.github.com>
2022-09-11 12:43:39 +0500

    refactor: made changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { ToastContainer } from "react-toastify";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <App />
       <ToastContainer />
     </BrowserRouter>
